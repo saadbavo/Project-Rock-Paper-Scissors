58e9b39e26ec0025e09beb0667eed948b279c049
saadbavo <[saadhmk13@gmail.com]>
2024-08-19 13:30:26 +0100

    Make 3 variables

--- javascript.js
+++ javascript.js
@@ -2,6 +2,10 @@
 function getComputerchoice(){   
 
       let randomNumber= Math.floor(Math.random()*3)
+      const choice1= "rock"
+      const choice2= "scissor"
+      const choice3= "paper"
+
       console.log(randomNumber)
 }
 getComputerchoice()
