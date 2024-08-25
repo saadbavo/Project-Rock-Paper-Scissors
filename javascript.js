b7895e5dca7c1b207560db69d8a9a7a7811888d0
saadbavo <[saadhmk13@gmail.com]>
2024-08-25 19:22:16 +0100

    Add condtion to diciede the  winer

--- javascript.js
+++ javascript.js
@@ -18,4 +18,46 @@ let humanScore = 0;
 let computerScore = 0;
 function playRound(humanChoice, computerChoice){
    humanChoice=humanChoice.toLocaleLowerCase()
+   if(humanChoice ==='rock'){
+      switch (computerChoice){
+         case 'scissor':
+            console.log("nice choice , you win!")
+            break
+         case 'paper':
+            console.log("you lose!try next time")
+            break
+         default:
+            console.log("it's a tie")            
+
+      }
+      
+   }
+   if(humanChoice ==='rock'){
+      switch (computerChoice){
+         case 'scissor':
+            console.log("nice choice , you win!")
+            break
+         case 'paper':
+            console.log("you lose!try next time")
+            break
+         default:
+            console.log("it's a tie")            
+
+      }
+      
+   }
+   if(humanChoice ==='rock'){
+      switch (computerChoice){
+         case 'scissor':
+            console.log("nice choice , you win!")
+            break
+         case 'paper':
+            console.log("you lose!try next time")
+            break
+         default:
+            console.log("it's a tie")            
+
+      }
+      
+   }
 }
