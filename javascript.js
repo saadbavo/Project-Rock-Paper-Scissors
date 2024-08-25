e7fbb1bfedf5c429860f9c3de3fe41b643e24ad7
saadbavo <[saadhmk13@gmail.com]>
2024-08-25 18:39:26 +0100

    Make sur input is one of the chices

--- javascript.js
+++ javascript.js
@@ -2,8 +2,16 @@ const choices = ["rock","paper", "scissor"]
 function getComputerChoice(){   
 
       return choices[Math.floor(Math.random()*3)] ;
+
 }
 function getHumanChoice(){
  let choice
- choice=prompt("rock, paper , scissor?:")
+ choice=window.prompt("rock, paper , scissor?:").toLocaleLowerCase();
+ if(choices.includes(choice)){
+   return choice;
+ }
+ else{
+   alert("invald input please try again")
+ }
 }
+getHumanChoice()
