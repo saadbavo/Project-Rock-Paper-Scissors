409223db824b663be43156360ebf39e1c59cbb16
saadbavo <[saadhmk13@gmail.com]>
2024-08-25 18:10:24 +0100

    redoing get computer choice function and delete all the athoercode

--- javascript.js
+++ javascript.js
@@ -1,26 +1,5 @@
-
+const choices = ["rock","paper", "scissor"]
 function getComputerChoice(){   
 
-      let randomNumber= Math.floor(Math.random()*3);
-      const choice1= "rock";
-      const choice2= "scissor";
-      const choice3= "paper";
-      if  (randomNumber==0){
-        return console.log(choice1);
-      }
-     else if (randomNumber==1){
-        return console.log(choice2);
-     }
-     else{
-        return console.log(choice3);
-     }
-}
-function  getHumanChoice(){
-let humanChoice=prompt("please chose one option: rock , paper , scissor").toLowerCase()
-return console.log(humanChoice)
-}
-humanScore=0;
-computerScore=0;
-function playRound(humanChoice,computerChoice){
-
+      return choices[Math.floor(Math.random()*3)] ;
 }
