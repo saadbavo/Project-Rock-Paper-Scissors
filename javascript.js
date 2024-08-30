49c814e6032984f5fd85f9145210489697cc820d
saadbavo <[saadhmk13@gmail.com]>
2024-08-30 18:29:30 +0100

    emove the logic that plays exactly five rounds

--- javascript.js
+++ javascript.js
@@ -4,14 +4,70 @@ function getComputerChoice(){
       return choices[Math.floor(Math.random()*3)] ;
       }
 
+      const allButton = document.querySelectorAll("#btn");
+   b.addEventListener('click', ()=> {
+    const palayerSelector= allButton.values;
 
-function getHumanChoice(){
- let choice
- choice=window.prompt("rock, paper , scissor?:").toLocaleLowerCase();
- if(choices.includes(choice)){
-   return choice;
- }
- else{
-   alert("invald input please try again");
- }
-}
+
+   })
+      computerSelector= getComputerChoice()
+      allButton.addEventListener("click", playRound(palayerSelector, computerSelector));
+      
+
+
+let  humanScore= 0;
+let computerScore=0;
+
+
+         function playRound(humanChoice, computerChoice){
+            humanChoice=humanChoice.toLocaleLowerCase()
+            if(humanChoice ==='rock'){
+               switch (computerChoice){
+                  case 'scissor':
+                     console.log("nice choice , you win!")
+                      humanScore++
+                     break
+                  case 'paper':
+                     console.log("you lose!try next time")
+                     computerScore++
+                     break
+                  default:
+                     console.log("it's a tie")            
+         
+               }
+               
+            }
+            if(humanChoice ==='paper'){
+               switch (computerChoice){
+                  case 'rock':
+                     console.log("nice choice , you win!")
+                     humanScore++
+                     break
+                  case 'scissor':
+                     console.log("you lose!try next time")
+                     computerScore++
+                     break
+                  default:
+                     console.log("it's a tie")            
+         
+               }
+               
+            }
+            if(humanChoice ==='scissor'){
+               switch (computerChoice){
+                  case 'paper':
+                     console.log("nice choice , you win!")
+                     humanScore++
+                     break
+                  case 'rock':
+                     console.log("you lose!try next time")
+                     computerScore++
+                     break
+                  default:
+                     console.log("it's a tie")            
+         
+               }
+               
+            }
+        }
+      
