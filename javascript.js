09cd2dbebdc1d7b32b32f3c0f7122fafb58d5d02
saadbavo <[saadhmk13@gmail.com]>
2024-10-03 18:35:28 +0100

    Add if statemanent whit foncution todecler human wins Add button for rstart game after winer dcler

--- javascript.js
+++ javascript.js
@@ -66,10 +66,29 @@ function playRound(humanChoice, computerChoice){
 const playerAndComputerScore= document.createElement("div");
 playerAndComputerScore.classList.add("scores");
 playerAndComputerScore.textContent = humanScore+' '+computerScore;
-score.appendChild(playerAndComputerScore)
+score.appendChild(playerAndComputerScore);
 
+if (humanScore== 5){
+   const finalScore = document.querySelector("#fscore");
+   const declare= document.createElement("h1");
+   declare.classList.add("dc");
+   declare.textContent = "Congelation! yOUR tHE wINNER"
+   finalScore.appendChild(declare)
+   const restBto  = document.createElement("button")
+   restBto.setAttribute("id", "rst")
+   restBto.textContent= "RESTART"
+   restBto.addEventListener('click',()=>{
+      location.reload()
+   })
+   finalScore.appendChild(restBto)
+}
+
+else if(computerScore==5){
+   cons
+}
 
 }
+
 
 
 const computerSelection = getComputerChoice()
